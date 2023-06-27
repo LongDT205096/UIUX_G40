6221fff374cdec891f2aa0784271510e6bb821ce
kristteesomr <84734773+kristteesomr@users.noreply.github.com>
2023-06-27 22:28:16 +0700

    Create message.py

---
+++ view/message.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template, flash, url_for, redirect, session
+
+message_bp = Blueprint('message', __name__)
+
+
+@message_bp.route('/')
+def chat():
+    return render_template('messages/message.html')
