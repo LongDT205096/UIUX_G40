e35ffe69abaa2164f91e0e0d5c9024b5e12acfac
kristteesomr <84734773+kristteesomr@users.noreply.github.com>
2023-06-27 22:28:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from config.app_config import Config
 from view.home import home_bp
 from view.auth import auth_bp
+from view.message import message_bp
 
 
 app = Flask(__name__)
@@ -10,6 +11,7 @@ app.config.from_object(Config)
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(home_bp)
+app.register_blueprint(message_bp)
 
 
 if __name__ == '__main__':
