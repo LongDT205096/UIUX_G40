b92cec99e71fc3c4cafcaff37d73dce03daa6994
thep2002 <59258172+thep2002@users.noreply.github.com>
2023-06-29 14:31:02 +0700

    task

--- config/app_config.py
+++ config/app_config.py
@@ -2,4 +2,4 @@ from datetime import timedelta
 
 class Config:
     SECRET_KEY = '20200563'
-    PERMANENT_SESSION_LIFETIME = timedelta(minutes=5)
+    PERMANENT_SESSION_LIFETIME = timedelta(minutes=60)
