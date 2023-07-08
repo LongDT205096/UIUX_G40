6b51e135d112e58c8a583bed9fa875a434c87d88
LongDT205096 <91714440+LongDT205096@users.noreply.github.com>
2023-07-08 14:40:24 +0700

    Delete view directory

--- view/project.py
+++
@@ -1,9 +0,0 @@
-import modal.project as modal_project
-
-
-def find_by_account_id(account_id):
-    return modal_project.find_by_account_id(account_id=account_id)
-
-
-def find_by_id(id):
-    return modal_project.find_by_id(id=id)
